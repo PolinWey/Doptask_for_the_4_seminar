4edc4dfa2f1f32296906327f8478adc9a9453b12
Polin <polina996@mail.ru>
2022-12-09 22:33:56 +0300

    push

---
+++ task30(original)/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 30 (ИСХОДНАЯ): Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+void arrayZero(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        col[i] = new Random().Next(0, 2);
+    }
+}
+void printArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write($"{col[i]} ");
+    }
+}
+int[] array = new int[8];
+arrayZero(array);
+printArray(array);
